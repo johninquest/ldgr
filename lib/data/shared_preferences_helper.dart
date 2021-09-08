72204acb1be5fdaea1817026b5ff19538d2cdf64
john@work <johninquest@gmail.com>
2021-09-08 12:27:53 +0200

    added shared preferences library

---
+++ lib/data/shared_preferences_helper.dart
@@ -0,0 +1,19 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesHelper {
+  Future saveData(String strKey, String strValue) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(strKey, strValue);
+  }
+
+  Future readData(String strKey) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final storedData = prefs.getString(strKey) ?? 'data not available';
+    return storedData;
+  }
+
+  Future removeData(String strKey) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove(strKey);
+  }
+}

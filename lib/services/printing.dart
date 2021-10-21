c626bdbbaf09fc103efb2a730e376d1da48d8520
john@work <johninquest@gmail.com>
2021-10-22 00:34:17 +0200

    trying out print packages

--- lib/services/printing.dart
+++ lib/services/printing.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
-import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pd;
 import 'package:path_provider/path_provider.dart';
+// import 'package:open_file/open_file.dart';
 
 class PrintService {
+  final String downloadDir = '/storage/emulated/0/Download/';
+
   initializeDir() async {
     Directory tempDir = await getTemporaryDirectory();
     // String tempPath = tempDir.path;
@@ -11,9 +13,22 @@ class PrintService {
     print('Temp path => ${tempDir.path}');
     return tempDir;
   }
-  
+
+  showPath() async {
+    // final Directory testDir = Directory('/storage/emulated/0/Download');
+    // final Directory? testDir = await getTemporaryDirectory();
+    final Directory? testDir = await getExternalStorageDirectory();
+    // final testDir = (await getExternalStorageDirectories(type: StorageDirectory.downloads))!.first;
+    print(testDir);
+    print(testDir!.path);
+  }
+
   saveAsPdf() async {
     final dir = await getTemporaryDirectory();
+    // final Directory? dir = await getExternalStorageDirectory();
+    // final Directory dir = Directory('/storage/emulated/0/Download');
+    // final dir = (await getExternalStorageDirectories(type: StorageDirectory.downloads))!.first;
+    // final String tempPath = dir!.path;
     print('Temp directory => $dir');
     print('Temp path => ${dir.path}');
     final pdf = pd.Document();
@@ -24,9 +39,11 @@ class PrintService {
         ),
       ),
     );
-    // final file = File('example.pdf');
+    // final file = File(downloadDir + 'example.pdf');
     final file = File('${dir.path}/example.pdf');
+    print(file);
     await file.writeAsBytes(await pdf.save());
+    // await OpenFile.open('$file');
   }
 
   saveToPrinter() {}

229dd85b8eaeb7f0b36410abefcad9e8a915c276
john@work <johninquest@gmail.com>
2022-01-03 23:03:48 +0100

    some more work on filter

--- lib/pages/records/analysis.dart
+++ lib/pages/records/analysis.dart
@@ -9,19 +9,35 @@ class AnalysisPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var _fsQuery = FirestoreService().getSubCollection();
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text('Analysis'),
         centerTitle: true,
       ),
-      body: AnalysisDashboard(),
+      // body: AnalysisDashboard(),
+      body: FutureBuilder(
+          future: _fsQuery,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return ErrorOccured();
+            }
+            if (snapshot.hasData) {
+              List resData = snapshot.data as List;
+              return AnalysisDashboard(fsData: resData);
+            } else {
+              return WaitingForResponse();
+            }
+          }),
       bottomNavigationBar: BottomNavBar(),
     );
   }
 }
 
 class AnalysisDashboard extends StatefulWidget {
-  const AnalysisDashboard({Key? key}) : super(key: key);
+  final List fsData;
+  const AnalysisDashboard({Key? key, required this.fsData}) : super(key: key);
 
   @override
   _AnalysisDashboardState createState() => _AnalysisDashboardState();
@@ -30,193 +46,215 @@ class AnalysisDashboard extends StatefulWidget {
 class _AnalysisDashboardState extends State<AnalysisDashboard> {
   @override
   Widget build(BuildContext context) {
-    var _fsQuery = FirestoreService().getSubCollection();
-    return FutureBuilder(
-      future: _fsQuery,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return ErrorOccured();
-        }
-        if (snapshot.hasData) {
-          List resData = snapshot.data as List;
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: EdgeInsets.only(bottom: 10.0),
-                child: Text(
-                  'OVERVIEW',
-                  style: TextStyle(color: myBlue, fontWeight: FontWeight.bold),
-                ),
+    final resData = widget.fsData;
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Container(
+        margin: EdgeInsets.only(left: 5.0, right: 5.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 10.0, top: 30.0),
+              child: Text(
+                '',
+                style: TextStyle(color: myBlue, fontWeight: FontWeight.bold),
               ),
-              Table(
-                // defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                // border: TableBorder.all(),
-                children: [
-                  TableRow(children: [
-                    TableCell(
-                        child: Container(
-                      margin:
-                          EdgeInsets.only(bottom: 1.0, top: 1.0, right: 2.0),
-                      color: myBlue,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(bottom: 5.0, top: 5.0),
-                            child: Text(
-                              'Total',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            ),
+            ),
+            Table(
+              // defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              // border: TableBorder.all(),
+              children: [
+                TableRow(children: [
+                  TableCell(
+                      child: Container(
+                    margin: EdgeInsets.only(bottom: 1.0, top: 1.0, right: 2.0),
+                    color: myBlue,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(bottom: 5.0, top: 5.0),
+                          child: Text(
+                            'Total',
+                            style: TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold),
                           ),
-                          Container(
-                            margin: EdgeInsets.only(bottom: 5.0, top: 5.0),
-                            child: Text(
-                              getSum(resData),
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          )
-                        ],
-                      ),
-                    )),
-                    TableDataBox(
-                      boxTitle: 'Paid',
-                      boxData: getSumByPaidStatus(resData, 'paid'),
-                      boxColor: myGreen,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(bottom: 5.0, top: 5.0),
+                          child: Text(
+                            getSum(resData),
+                            style: TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
                     ),
-                    TableDataBox(
-                      boxTitle: 'Unpaid',
-                      boxData: getSumByPaidStatus(resData, 'unpaid'),
-                      boxColor: myAmber,
+                  )),
+                  TableDataBox(
+                    boxTitle: 'Paid',
+                    boxData: getSumByPaidStatus(resData, 'paid'),
+                    boxColor: myGreen,
+                  ),
+                  TableDataBox(
+                    boxTitle: 'Unpaid',
+                    boxData: getSumByPaidStatus(resData, 'unpaid'),
+                    boxColor: myAmber,
+                  ),
+                ]),
+                TableRow(children: [
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: 'Bar',
+                    boxColor: Colors.white,
+                  ),
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData:
+                        getSumByPaidStatusAndCostArea(resData, 'paid', 'bar'),
+                    boxColor: myGreen,
+                  ),
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData:
+                        getSumByPaidStatusAndCostArea(resData, 'unpaid', 'bar'),
+                    boxColor: myAmber,
+                  ),
+                ]),
+                TableRow(children: [
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: 'Cleaning',
+                    boxColor: Colors.white,
+                  ),
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: getSumByPaidStatusAndCostArea(
+                        resData, 'paid', 'cleaning'),
+                    boxColor: myGreen,
+                  ),
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: getSumByPaidStatusAndCostArea(
+                        resData, 'unpaid', 'cleaning'),
+                    boxColor: myAmber,
+                  ),
+                ]),
+                TableRow(children: [
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: 'Kitchen',
+                    boxColor: Colors.white,
+                  ),
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData:
+                        getSumByPaidStatusAndCostArea(resData, 'paid', 'kitchen'),
+                    boxColor: myGreen,
+                  ),
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: getSumByPaidStatusAndCostArea(
+                        resData, 'unpaid', 'kitchen'),
+                    boxColor: myAmber,
+                  ),
+                ]),
+                TableRow(children: [
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: 'Operations',
+                    boxColor: Colors.white,
+                  ),
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: getSumByPaidStatusAndCostArea(
+                        resData, 'paid', 'operation'),
+                    boxColor: myGreen,
+                  ),
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: getSumByPaidStatusAndCostArea(
+                        resData, 'unpaid', 'operation'),
+                    boxColor: myAmber,
+                  ),
+                ]),
+                TableRow(children: [
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: 'Other(s)',
+                    boxColor: Colors.white,
+                  ),
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData:
+                        getSumByPaidStatusAndCostArea(resData, 'paid', 'others'),
+                    boxColor: myGreen,
+                  ),
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: getSumByPaidStatusAndCostArea(
+                        resData, 'unpaid', 'others'),
+                    boxColor: myAmber,
+                  ),
+                ]),
+                TableRow(children: [
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: 'Toilet',
+                    boxColor: Colors.white,
+                  ),
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData:
+                        getSumByPaidStatusAndCostArea(resData, 'paid', 'toilet'),
+                    boxColor: myGreen,
+                  ),
+                  TableDataBox(
+                    boxTitle: '',
+                    boxData: getSumByPaidStatusAndCostArea(
+                        resData, 'unpaid', 'toilet'),
+                    boxColor: myAmber,
+                  ),
+                ]),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                      width: MediaQuery.of(context).size.width * 0.15,
+                      margin: EdgeInsets.only(bottom: 10.0, top: 10.0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        maxLength: 2,
+                        decoration: InputDecoration(labelText: 'Month'),
+                      )),
+                  Container(
+                      width: MediaQuery.of(context).size.width * 0.20, 
+                     
+                      margin: EdgeInsets.only(bottom: 10.0, top: 10.0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        maxLength: 4,
+                        decoration: InputDecoration(labelText: 'Year'),
+                      )),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.18, 
+                    margin: EdgeInsets.only(bottom: 10.0, top: 10.0),
+                    child: ElevatedButton(
+                      child: Text('Filter'),
+                      onPressed: () => print('Tapped button!'),
                     ),
-                  ]),
-                  TableRow(children: [
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: 'Bar',
-                      boxColor: Colors.white,
-                    ),
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData:
-                          getSumByPaidStatusAndCostArea(resData, 'paid', 'bar'),
-                      boxColor: myGreen,
-                    ),
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: getSumByPaidStatusAndCostArea(
-                          resData, 'unpaid', 'bar'),
-                      boxColor: myAmber,
-                    ),
-                  ]),
-                  TableRow(children: [
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: 'Cleaning',
-                      boxColor: Colors.white,
-                    ),
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: getSumByPaidStatusAndCostArea(
-                          resData, 'paid', 'cleaning'),
-                      boxColor: myGreen,
-                    ),
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: getSumByPaidStatusAndCostArea(
-                          resData, 'unpaid', 'cleaning'),
-                      boxColor: myAmber,
-                    ),
-                  ]),
-                  TableRow(children: [
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: 'Kitchen',
-                      boxColor: Colors.white,
-                    ),
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: getSumByPaidStatusAndCostArea(
-                          resData, 'paid', 'kitchen'),
-                      boxColor: myGreen,
-                    ),
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: getSumByPaidStatusAndCostArea(
-                          resData, 'unpaid', 'kitchen'),
-                      boxColor: myAmber,
-                    ),
-                  ]),
-                  TableRow(children: [
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: 'Operations',
-                      boxColor: Colors.white,
-                    ),
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: getSumByPaidStatusAndCostArea(
-                          resData, 'paid', 'operation'),
-                      boxColor: myGreen,
-                    ),
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: getSumByPaidStatusAndCostArea(
-                          resData, 'unpaid', 'operation'),
-                      boxColor: myAmber,
-                    ),
-                  ]),
-                  TableRow(children: [
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: 'Other(s)',
-                      boxColor: Colors.white,
-                    ),
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: getSumByPaidStatusAndCostArea(
-                          resData, 'paid', 'others'),
-                      boxColor: myGreen,
-                    ),
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: getSumByPaidStatusAndCostArea(
-                          resData, 'unpaid', 'others'),
-                      boxColor: myAmber,
-                    ),
-                  ]),
-                  TableRow(children: [
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: 'Toilet',
-                      boxColor: Colors.white,
-                    ),
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: getSumByPaidStatusAndCostArea(
-                          resData, 'paid', 'toilet'),
-                      boxColor: myGreen,
-                    ),
-                    TableDataBox(
-                      boxTitle: '',
-                      boxData: getSumByPaidStatusAndCostArea(
-                          resData, 'unpaid', 'toilet'),
-                      boxColor: myAmber,
-                    ),
-                  ]),
+                  ),
                 ],
-              ), 
-              // FilterForm(),
-            ],
-          );
-        } else {
-          return WaitingForResponse();
-        }
-      },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -267,7 +305,7 @@ class TableDataBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return TableCell(
       child: Container(
-        margin: EdgeInsets.only(bottom: 1.0, top: 1.0),
+        margin: EdgeInsets.only(bottom: 1.0, top: 1.0, left: 3.0, right: 3.0),
         decoration: BoxDecoration(
             color: boxColor,
             borderRadius: BorderRadius.all(Radius.circular(0.0))),
@@ -291,59 +329,3 @@ class TableDataBox extends StatelessWidget {
   }
 }
 
-class FilterForm extends StatefulWidget {
-  const FilterForm({Key? key}) : super(key: key);
-
-  @override
-  _FilterFormState createState() => _FilterFormState();
-}
-
-class _FilterFormState extends State<FilterForm> {
-  // final _filterFormKey = GlobalKey<FormState>();
-  TextEditingController _monthNumber = TextEditingController();
-  TextEditingController _yearNumber = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-       width: MediaQuery.of(context).size.width * 0.90, 
-       height: 15.0,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-        Container(
-          width: MediaQuery.of(context).size.width * 0.30, 
-          height: 10.0,
-          child: TextFormField(
-            controller: _monthNumber,
-            enabled: true,
-            decoration: InputDecoration(labelText: 'Month'),
-            keyboardType: TextInputType.number,
-            maxLength: 2,
-            // maxLengthEnforcement: MaxLengthEnforcement.enforced,
-          ),
-        ),
-         Container(
-           width: MediaQuery.of(context).size.width * 0.30, 
-           height: 10.0,
-          child: TextFormField(
-            controller: _yearNumber,
-            enabled: true,
-            decoration: InputDecoration(labelText: 'Month'),
-            keyboardType: TextInputType.number,
-            maxLength: 4,
-          ),
-        ),
-        Container(
-           width: MediaQuery.of(context).size.width * 0.20, 
-           height: 10.0,
-          child: ElevatedButton(
-            onPressed: () => print('Button tapped'),
-            child: Text('Go'),
-            style: ElevatedButton.styleFrom(primary: myBlue),
-          ),
-        )
-      ]),
-    );
-  }
-}
--- lib/services/filter.dart
+++ lib/services/filter.dart
@@ -1,11 +1,25 @@
+import 'package:intl/intl.dart';
 
 class ListFilterService {
-
-  byDate() {} 
+  byDate(List filterList, String filterDate) {
+    DateFormat targetFormat = DateFormat('yyyy-MM-dd');
+    List filtered = [];
+    for (var i in filterList) {
+      if (i['created_at'] == filterDate) {
+        filtered.add(i);
+      }
+    }
+    return filtered;
+  }
 
   byPeriod() {}
 
-  byCostArea() {} 
+  byMonthAndYear(List filterList, int filterMonth, int filterYear) {
+    List filtered = [];
+    for (var i in filterList) {}
+  }
+
+  byCostArea() {}
 
   byPaymentStatus() {}
 }

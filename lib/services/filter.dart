37d6811caa576c3d438bc93f2e95d75a1ee4eb83
john@work <johninquest@gmail.com>
2022-01-11 13:07:24 +0100

    added filter funtion byString to enable filtering across all fields

--- lib/services/filter.dart
+++ lib/services/filter.dart
@@ -1,6 +1,21 @@
 import 'package:intl/intl.dart';
 
 class ListFilterService {
+  byString(List filterList, String targetStr) {
+    List filtered = [];
+    for (var i in filterList) {
+      if (i['cost_area'] == targetStr ||
+          i['item_category'] == targetStr ||
+          i['item_name'] == targetStr ||
+          i['entered_by'] == targetStr ||
+          i['payment_method'] == targetStr ||
+          i['payment_status'] == targetStr) {
+        filtered.add(i);
+      }
+    }
+    return filtered;
+  }
+
   byDate(List filterList, String filterDate) {
     // DateFormat targetFormat = DateFormat('yyyy-MM-dd');
     List filtered = [];

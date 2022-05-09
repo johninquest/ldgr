6df446c5b25a0bec4300e51b114ed39012d1d376
john@work <johninquest@gmail.com>
2022-05-09 13:40:07 +0200

    added remember me button

--- lib/pages/inputs/login.dart
+++ lib/pages/inputs/login.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:ldgr/db/sp_helper.dart';
 // import 'package:ldgr/firebase/auth.dart';
 import 'package:ldgr/firebase/firestore.dart';
@@ -20,7 +21,7 @@ class LoginPage extends StatelessWidget {
         title: Text('Ldgr',
             style: TextStyle(
               fontWeight: FontWeight.bold,
-              letterSpacing: 3.0,
+              letterSpacing: 1.0,
               color: Colors.white,
             )),
         centerTitle: true,
@@ -42,129 +43,148 @@ class _LoginFormState extends State<LoginForm> {
   final _loginFormKey = GlobalKey<FormState>();
   TextEditingController _userName = TextEditingController();
   TextEditingController _userPassword = TextEditingController();
+  bool? isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _loginFormKey,
       child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            /* Container(
-              child: Text(
-                'The bookkeeping app for bars and restaurants',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold, color: myBlue, fontSize: 16.0),
-              ),
-            ), */
-            Container(
-                width: MediaQuery.of(context).size.width * 0.75,
-                margin: EdgeInsets.only(bottom: 10.0, top: 40.0),
-                padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                child: TextFormField(
-                  controller: _userName,
-                  decoration: InputDecoration(labelText: 'UserId'),
-                  keyboardType: TextInputType.text,
-                  validator: (val) {
-                    if (val == null || val.isEmpty) {
-                      return 'Please enter UserId';
-                    }
-                  },
-                )),
-            Container(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  width: MediaQuery.of(context).size.width * 0.75,
+                  margin: EdgeInsets.only(bottom: 10.0, top: 40.0),
+                  padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: TextFormField(
+                    controller: _userName,
+                    decoration: InputDecoration(labelText: 'UserId'),
+                    keyboardType: TextInputType.text,
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return 'Please enter UserId';
+                      }
+                    },
+                  )),
+              Container(
+                  width: MediaQuery.of(context).size.width * 0.75,
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: TextFormField(
+                    controller: _userPassword,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: InputDecoration(labelText: 'Password'),
+                    keyboardType: TextInputType.text,
+                    enabled: true,
+                    /*                   validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return 'Please enter password';
+                      }
+                    }, */
+                  )),
+              Container(
                 width: MediaQuery.of(context).size.width * 0.75,
                 margin: EdgeInsets.only(bottom: 20.0),
-                padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                child: TextFormField(
-                  controller: _userPassword,
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: InputDecoration(labelText: 'Password'),
-                  keyboardType: TextInputType.text, 
-                  enabled: true,
-/*                   validator: (val) {
-                    if (val == null || val.isEmpty) {
-                      return 'Please enter password';
-                    }
-                  }, */
-                )),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.50,
-              margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  String userNameCleanedUp =
-                      _userName.text.trim().toLowerCase();
-                  String userPasswordCleanedUp =
-                      _userPassword.text.trim().toLowerCase();
-                  if (_loginFormKey.currentState!.validate()) {
-                    var adminAuth = AuthService()
-                        .verifyAdmin(userNameCleanedUp, userPasswordCleanedUp);
-                    if (adminAuth == 'auth_success') {
-                      String name = 'admin';
-                      String role = 'admin';
-                      String businessName = '';
-                      String businessLocation = '';
-                      storeCurrentUser(
-                          name, role, businessName, businessLocation);
-                      PageRouter().navigateToPage(HomePage(), context);
-                    } else {
-                      FirestoreService()
-                          .checkIfDocExists(userNameCleanedUp)
-                          .then((DocumentSnapshot snapshot) {
-                        if (snapshot.exists) {
-                          Map<String, dynamic> _data =
-                              snapshot.data() as Map<String, dynamic>;
-                          String name = _data['name'] ?? '';
-                          String role = _data['role'] ?? '';
-                          String businessName = _data['business_name'] ?? '';
-                          String businessLocation = _data['city'] ?? '';
-                          storeCurrentUser(
-                              name, role, businessName, businessLocation);
-                          PageRouter().navigateToPage(HomePage(), context);
-                        } else {
-                          showDialog(
-                              context: context,
-                              builder: (_) => ErrorDialog('Access denied!'));
-                        }
-                      }).catchError((e) {
-                        showDialog(
-                            context: context,
-                            builder: (_) => ErrorDialog(
-                                'Something went wrong.\n Please inform your manager!'));
-                      });
-                    }
-
-                    /* var adminAuth = AdminAuthService()
-                        .verifyAdmin(_userName.text, _userPassword.text);
-                    if (adminAuth == 'auth_success') {
-                      PageRouter().navigateToPage(HomePage(), context);
-                    } else {
-                      var _authUser = FirebaseAuthService().loginUser(
-                          _userName.text.trim(), _userPassword.text.trim());
-                      _authUser.then((val) {
-                        if (val == null) {
-                          showDialog(
-                              context: context,
-                              builder: (_) => ErrorDialog('Access denied!'));
-                        } else {
-                          PageRouter().navigateToPage(HomePage(), context);
-                        }
-                      });
-                    } */
-                  }
-                },
-                child: Text(
-                  'Log in',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                style: ElevatedButton.styleFrom(
-                  primary: myBlue,
+                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                child: Row(
+                  children: [
+                    Checkbox(
+                      checkColor: Colors.white,
+                      // fillColor: MaterialStateProperty.resolveWith(getColor),
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value as bool;
+                          print('Remember me state => $isChecked');
+                        });
+                      },
+                    ),
+                    Text('Remember me'),
+                  ],
                 ),
               ),
-            ),
-          ],
+              Container(
+                width: MediaQuery.of(context).size.width * 0.50,
+                margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    String userNameCleanedUp =
+                        _userName.text.trim().toLowerCase();
+                    String userPasswordCleanedUp =
+                        _userPassword.text.trim().toLowerCase();
+                    if (_loginFormKey.currentState!.validate()) {
+                      var adminAuth = AuthService().verifyAdmin(
+                          userNameCleanedUp, userPasswordCleanedUp);
+                      if (adminAuth == 'auth_success') {
+                        String name = 'admin';
+                        String role = 'admin';
+                        String businessName = '';
+                        String businessLocation = '';
+                        storeCurrentUser(
+                            name, role, businessName, businessLocation);
+                        PageRouter().navigateToPage(HomePage(), context);
+                      } else {
+                        FirestoreService()
+                            .checkIfDocExists(userNameCleanedUp)
+                            .then((DocumentSnapshot snapshot) {
+                          if (snapshot.exists) {
+                            Map<String, dynamic> _data =
+                                snapshot.data() as Map<String, dynamic>;
+                            String name = _data['name'] ?? '';
+                            String role = _data['role'] ?? '';
+                            String businessName = _data['business_name'] ?? '';
+                            String businessLocation = _data['city'] ?? '';
+                            storeCurrentUser(
+                                name, role, businessName, businessLocation);
+                            PageRouter().navigateToPage(HomePage(), context);
+                          } else {
+                            showDialog(
+                                context: context,
+                                builder: (_) => ErrorDialog('Access denied!'));
+                          }
+                        }).catchError((e) {
+                          showDialog(
+                              context: context,
+                              builder: (_) => ErrorDialog(
+                                  'Something went wrong.\n Please inform your manager!'));
+                        });
+                      }
+
+                      /* var adminAuth = AdminAuthService()
+                          .verifyAdmin(_userName.text, _userPassword.text);
+                      if (adminAuth == 'auth_success') {
+                        PageRouter().navigateToPage(HomePage(), context);
+                      } else {
+                        var _authUser = FirebaseAuthService().loginUser(
+                            _userName.text.trim(), _userPassword.text.trim());
+                        _authUser.then((val) {
+                          if (val == null) {
+                            showDialog(
+                                context: context,
+                                builder: (_) => ErrorDialog('Access denied!'));
+                          } else {
+                            PageRouter().navigateToPage(HomePage(), context);
+                          }
+                        });
+                      } */
+                    }
+                  },
+                  child: Text(
+                    'Log in',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: myBlue,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

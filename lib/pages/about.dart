dbfbe764fe8406225525e4c06df4169bb8d59af6
john@work <johninquest@gmail.com>
2022-05-19 12:34:57 +0200

    now displaying app version

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ldgr/services/web.dart';
+import 'package:ldgr/shared/app_info.dart';
 import 'package:ldgr/shared/bottom_nav_bar.dart';
 import 'package:ldgr/styles/colors.dart';
 import 'package:ldgr/styles/style.dart';
@@ -21,13 +22,15 @@ class AboutPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Container(
+        width: MediaQuery.of(context).size.width * 0.95,
+        height: MediaQuery.of(context).size.width * 0.95,
         child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                margin: EdgeInsets.only(bottom: 10.0),
+                margin: EdgeInsets.only(bottom: 5.0),
                 child: Text(
                   'Ldgr',
                   style: TextStyle(
@@ -36,15 +39,17 @@ class AboutPage extends StatelessWidget {
                       letterSpacing: 2.0),
                 ),
               ),
+              Divider(indent: 100.0, endIndent: 100.0,),
               Container(
-                margin: EdgeInsets.only(left: 10.0, right: 10.0),
+                margin: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 100.0),
                 child: Text(
-                  'Ldgr is a simple bookkeeping app for bars and restaurants.',
+                  'Ldgr is a simple bookkeeping app for your catering business.',
                   textAlign: TextAlign.center,
                 ),
               ),
+              // SizedBox(height: 50.0,),
               Container(
-                margin: EdgeInsets.only(top: 100.0),
+                margin: EdgeInsets.only(top: 10.0),
                 child: GestureDetector(
                   onTap: () => WebService().openUrl('https://johnapps.de', context),
                   child: Text('\u00A9 $_currentYear JOHN APPS', 
@@ -52,10 +57,12 @@ class AboutPage extends StatelessWidget {
                     // fontStyle: FontStyle.italic, 
                     color: myBlue,
                     fontWeight: FontWeight.bold, 
-                    decoration: TextDecoration.underline
+                    // decoration: TextDecoration.underline
                     ),),
                 ),
-              )
+              ),
+              SizedBox(height: 30.0,),
+              AppInfo()
             ],
           ),
         ),
---
+++ lib/shared/app_info.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import '../services/web.dart';
+import '../styles/colors.dart';
+
+class AppInfo extends StatelessWidget {
+  const AppInfo({Key? key}) : super(key: key);
+
+  Future<PackageInfo> _getPackageInfo() {
+    return PackageInfo.fromPlatform();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FutureBuilder<PackageInfo>(
+            future: _getPackageInfo(),
+            builder:
+                (BuildContext context, AsyncSnapshot<PackageInfo> snapshot) {
+              if (snapshot.hasError) {
+                // print('Error => $snapshot');
+                return Text('An error occured!');
+              } else if (snapshot.hasData) {
+                final _data = snapshot.data!;
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                   /*  Text('App name: ${_data.appName}'),
+                    SizedBox(
+                      height: 3.0,
+                    ),
+                    Text('Package name: ${_data.packageName}'), 
+                    SizedBox(
+                      height: 3.0,
+                    ), */
+                    Text('App version: ${_data.version}'),
+                    SizedBox(
+                      height: 3.0,
+                    ),
+                    Text('Build number: ${_data.buildNumber}'),
+/*                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('App version: ${_data.version}'),
+                        Text('Build number: ${_data.buildNumber}'),
+                      ],
+                    ) */
+                  ],
+                );
+              } else {
+                return CircularProgressIndicator();
+              }
+            }),
+      ),
+    );
+  }
+}
